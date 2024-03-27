b42c547e18e50bbb01011e5a05f11894ec69e0dd
Adesh Bharane <95290497+aadibharane@users.noreply.github.com>
2024-03-27 14:18:15 +0530

    Add files via upload

---
+++ machine_test/api/views.py
@@ -0,0 +1,19 @@
+from rest_framework import viewsets
+from .models import Client, Project
+from .serializers import ClientSerializer, ProjectSerializer
+
+
+class ClientViewSet(viewsets.ModelViewSet):
+    
+    queryset = Client.objects.all()
+    serializer_class = ClientSerializer
+
+class ProjectViewSet(viewsets.ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+
+    def get_queryset(self):
+        #user = self.request.user
+        #return Project.objects.filter(users=user)
+        return Project.objects.all()
+    
