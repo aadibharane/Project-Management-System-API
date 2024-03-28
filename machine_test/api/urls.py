c56b129aac640fc1cf08598bfe676abb1e13d4d9
Adesh Bharane <95290497+aadibharane@users.noreply.github.com>
2024-03-28 17:39:01 +0530

    Update urls.py

--- machine_test/api/urls.py
+++ machine_test/api/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import ClientViewSet, ProjectViewSet 
+from api import views
 router = DefaultRouter()
 router.register(r'clients', ClientViewSet)
 router.register(r'projects', ProjectViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('create/',views.CreateAPIView.as_view(),name='client_create'),
     
 ]
