cd7cb38385a5ecf66e9eea61e8bc7adb2e7c4823
Adesh Bharane <95290497+aadibharane@users.noreply.github.com>
2024-03-28 16:56:41 +0530

    Add files via upload

---
+++ machine_test/api/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Client(models.Model):
+    client_name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.client_name
+
+class Project(models.Model):
+    project_name = models.CharField(max_length=100)
+    client = models.ForeignKey(Client, related_name='projects', on_delete=models.CASCADE,null=True)
+    users = models.ManyToManyField(User)
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='projects')
+
+    def __str__(self):
+        return self.project_name
+
---
+++ machine_test/api/views.py
@@ -0,0 +1,25 @@
+from rest_framework import viewsets
+from rest_framework.generics import DestroyAPIView
+from .models import Client, Project
+from .serializers import ClientSerializer, ProjectSerializer
+from rest_framework.generics import DestroyAPIView
+
+class ClientViewSet(viewsets.ModelViewSet):
+    
+    queryset = Client.objects.all()
+    serializer_class = ClientSerializer
+
+# class ClientDeleteView(DestroyAPIView):
+#     queryset = Client.objects.all()
+#     serializer_class = ClientSerializer
+
+
+class ProjectViewSet(viewsets.ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+
+    def get_queryset(self):
+        #user = self.request.user
+        #return Project.objects.filter(users=user)
+        return Project.objects.all()
+    
