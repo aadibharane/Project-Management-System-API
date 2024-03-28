13cfe4fb64762ce7df78e2f7c273281beb180436
Adesh Bharane <95290497+aadibharane@users.noreply.github.com>
2024-03-28 17:38:13 +0530

    Update views.py

--- machine_test/api/views.py
+++ machine_test/api/views.py
@@ -2,13 +2,17 @@ from rest_framework import viewsets
 from rest_framework.generics import DestroyAPIView
 from .models import Client, Project
 from .serializers import ClientSerializer, ProjectSerializer
-from rest_framework.generics import DestroyAPIView
+from rest_framework.generics import CreateAPIView
 
 class ClientViewSet(viewsets.ModelViewSet):
     
     queryset = Client.objects.all()
     serializer_class = ClientSerializer
 
+class CreateProjectAPIView(CreateAPIView):
+    """This endpoint allows for creation of a todo"""
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
 
 class ProjectViewSet(viewsets.ModelViewSet):
     queryset = Project.objects.all()
